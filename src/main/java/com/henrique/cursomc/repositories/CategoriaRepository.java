15e504147a4f5f968ef8ca7a9a6072fc91c8f76d
Henrique <cassio.henrique14@gmail.com>
2022-03-06 00:07:03 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/henrique/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.henrique.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.henrique.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
